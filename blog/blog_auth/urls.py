d6bcbd6486e3fe37b85146299de89cd4db1a1141
juice.andy <juice.j.andy@gmail.com>
2023-04-07 20:49:09 +0300

    feat: add registration

--- blog/blog_auth/urls.py
+++ blog/blog_auth/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog_auth'
 urlpatterns = [
     path('login/', views.login_page, name='login_page'),
     path('logout/', views.logout_page, name='logout_page'),
+    path('registration/', views.registration_page, name='registration_page'),
 ]
--- blog/blog_auth/views.py
+++ blog/blog_auth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 
 def login_page(request):
@@ -21,3 +22,14 @@ def logout_page(request):
     logout(request)
     return redirect('posts:home')
 
+
+def registration_page(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('posts:home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog_auth/registration_page.html', {'form': form})
