33754b2971564bae6e4206087222591f62258225
juice.andy <juice.j.andy@gmail.com>
2023-05-19 16:15:53 +0300

    add: Post Update/Create/Delete views, CommentDeleteView, ProfileUpdateView

--- blog/blog_edit/urls.py
+++ blog/blog_edit/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 from . import views
+from .views import *
 
 app_name = 'blog_edit'
 
 urlpatterns = [
-    path('add_new_post/', views.create_page, name='create_post_page'),
-    path('change_post/<int:pk>/', views.change_post, name='change_post'),
-    path('delete_post/<int:pk>/', views.delete_post, name='delete_post'),
-    path('del_comment/<int:pk>/', views.del_comment, name='del_comment'),
-    path('edit_user/<int:pk>/', views.edit_user, name='edit_user'),
+    # path('add_new_post/', views.create_page, name='create_post_page'),
+    path('add_new_post/', CreatePostView.as_view(), name='create_post_page'),
+    # path('change_post/<int:pk>/', views.change_post, name='change_post'),
+    path('change_post/<int:pk>/', views.PostUpdateView.as_view(), name='change_post'),
+    # path('delete_post/<int:pk>/', views.delete_post, name='delete_post'),
+    path('delete_post/<int:pk>/', views.PostDeleteView.as_view(), name='delete_post'),
+    # path('del_comment/<int:pk>/', views.del_comment, name='del_comment'),
+    path('del_comment/<int:pk>/', views.CommentDeleteView.as_view(), name='del_comment'),
+    # path('edit_user/<int:pk>/', views.edit_user, name='edit_user'),
+    path('edit_user/<int:pk>/', views.ProfileUpdateView.as_view(), name='edit_user'),
     path('delete_profile/', views.delete_profile, name='delete_profile'),
+    # path('delete_profile/', views.ProfileDeleteView.as_view(), name='delete_profile')
 ]
--- blog/blog_edit/views.py
+++ blog/blog_edit/views.py
@@ -1,66 +1,119 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.urls import reverse_lazy
+
 from blog_posts.models import Post, Comment
 from .forms import PostForm, CommentForm, UserForm
 from django.contrib.auth import get_user_model
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 User = get_user_model()
 
 
-def create_page(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.profile = request.user
-            post.save()
-            return redirect('blog_posts:posts_page')
-    else:
-        form = PostForm()
-    return render(request, 'blog_edit/add_post_page.html', {'form': form})
+# def create_page(request):
+#     if request.method == 'POST':
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.profile = request.user
+#             post.save()
+#             return redirect('blog_posts:posts_page')
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog_edit/add_post_page.html', {'form': form})
+
+class CreatePostView(CreateView):
+    model = Post
+    template_name = 'blog_edit/add_post_page.html'
+    form_class = PostForm
+    success_url = reverse_lazy('blog_posts:posts_page')
+
+    def form_valid(self, form):
+        form.instance.profile = self.request.user
+        return super().form_valid(form)
 
 
-def change_post(request, pk):
-    post = Post.objects.get(pk=pk)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-            messages.success(request, f'The post is update!')
-            return redirect('blog_posts:posts_page')
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog_edit/edit_post.html', {'form': form})
+# def change_post(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, instance=post)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, f'The post is update!')
+#             return redirect('blog_posts:posts_page')
+#     else:
+#         form = PostForm(instance=post)
+#     return render(request, 'blog_edit/edit_post.html', {'form': form})
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog_edit/edit_post.html'
+    success_url = reverse_lazy('blog_posts:posts_page')
 
 
-def delete_post(request, pk):
-    post = Post.objects.get(pk=pk)
-    post.delete()
-    return redirect('blog_posts:posts_page')
+# def delete_post(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     post.delete()
+#     return redirect('blog_posts:posts_page')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog_posts:posts_page')
+
+    def get(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
 
 
-def del_comment(request, pk):
-    print(pk)
-    comment = Comment.objects.get(pk=pk)
-    print(comment)
-    comment.delete()
-    return redirect(request.META.get('HTTP_REFERER'))
+# def del_comment(request, pk):
+#     print(pk)
+#     comment = Comment.objects.get(pk=pk)
+#     print(comment)
+#     comment.delete()
+#     return redirect(request.META.get('HTTP_REFERER'))
 
 
-def edit_user(request, pk):
-    user = User.objects.get(pk=pk)
-    if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
-        if form.is_valid():
-            form.save()
-            return redirect('blog_posts:posts_page')
-    else:
-        form = UserForm(instance=user)
-    return render(request, 'blog_edit/edit_user.html', {'form': form})
+class CommentDeleteView(DeleteView):
+    model = Comment
 
+    def get_success_url(self):
+        comment = self.get_object()
+        post = comment.post
+        return reverse_lazy('blog_posts:single_post', kwargs={'pk': post.pk})
+
+    def get(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
+
+
+# def edit_user(request, pk):
+#     user = User.objects.get(pk=pk)
+#     if request.method == 'POST':
+#         form = UserForm(request.POST, instance=user)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('blog_posts:posts_page')
+#     else:
+#         form = UserForm(instance=user)
+#     return render(request, 'blog_edit/edit_user.html', {'form': form})
+
+class ProfileUpdateView(UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = 'blog_edit/edit_user.html'
+    success_url = reverse_lazy('blog_posts:posts_page')
 
 def delete_profile(request):
     if request.method == 'POST':
         request.user.delete()
         return redirect('blog_posts:home_page')
     return render(request, 'blog_edit/delete_profile.html')
+
+
+# class ProfileDeleteView(DeleteView):
+#     model = User
+#     template_name = 'blog_edit/delete_profile.html'
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.delete(request, *args, **kwargs)
+
+
